769e113647ceae0572f1932f79372da9fae5b763
Taylor Smith <t.m.smith030@gmail.com>
2018-10-06 18:40:15 -0500

    Sets up styled components linting

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,5 @@
+jest.unmock('./App')
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
---
+++ src/sidebar/SidebarPanel.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+export class SidebarPanel extends Component {
+
+    render() {
+        <div/>;
+    }
+}
